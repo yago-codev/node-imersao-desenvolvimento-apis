5a680308160eacf0a73271ffa64da4f2c8b47409
Yago Milano <yago.goncalves@invillia.com>
2022-02-27 04:11:58 -0300

    modulo 02/aula01: for, for in, for of e map (medindo performance)

---
+++ modulo02-manipulacao-de-listas/manipulando-listas/for-forin/service.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+const URL = `https://swapi.dev/api/people`
+
+async function getPeoples(name) {
+  const url = `${URL}/?search=${name}&format=json`
+  console.log(url)
+  const getData = axios.get(url)
+  const response = await getData
+  return response.data.results
+}
+
+// getPeoples('r2')
+//   .then(function(result) {
+//     console.log(`Resultado: ${result.data.results[0].name}`)
+//   })
+//   .catch(function(error) {
+//     console.error(`Deu ruim: ${error} 💣`)
+//   })
+
+module.exports = {
+  getPeoples
+}
