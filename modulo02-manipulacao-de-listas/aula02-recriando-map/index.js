3c6d4efecb1c5bc337c0fc795ff45a0ae237640d
Yago Milano <yago.goncalves@invillia.com>
2022-02-27 20:05:19 -0300

    renomeando pasta

---
+++ modulo02-manipulacao-de-listas/aula02-recriando-map/index.js
@@ -0,0 +1,39 @@
+const service = require('../aula01-manipulando-listas/for-forin-forof-map/service')
+
+Array.prototype.myMap = function(callback) {
+  const newMappedArray = []
+  for(let index = 0; index <= this.length - 1; index++) {
+    const result = callback(this[index], index, newMappedArray)
+    newMappedArray.push(result)
+  }
+
+  return newMappedArray
+}
+
+async function main() {
+  try {
+    const results = await service.getPeoples('a')
+    // const names = []
+
+    // results.forEach(function(item) {
+    //   names.push(item.name)
+    // })
+
+    // const names = results.map(function(person) {
+    //   return person.name
+    // })
+
+    // const names = results.map(({ name }) => name)
+    const names = results.myMap(({ name }, index) => {
+      return {
+        index: `${index}`,
+        name: `${name}`,
+      }
+    })
+    console.log('Resultado: ', names)
+  } catch (error) {
+    console.error(`Deu ruim ${error} 💣`)
+  }
+}
+
+main()
